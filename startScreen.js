5110e1c62e56dd5b2cea1c57df18bac8519a12d2
whosfox-dev <fox.w@comcast.net>
2025-09-25 12:20:05 -0700

    Updated Home Page UI

--- startScreen.js
+++ startScreen.js
@@ -4,7 +4,14 @@ export default class startScreen extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('background', 'assets/background.webp');
+    this.load.image('background', 'assets/Davis streets-fotor-ai-art-effects-20250918131614.png');
+    this.load.image('btnPlay', 'assets/play.png');
+    this.load.image('btnTrophy', 'assets/prize.png');
+    this.load.image('btnSettings', 'assets/setting.png');
+    this.load.spritesheet('frog', 'assets/frog_idle_sheet_horizontal.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    });
   }
 
   create() {
@@ -13,16 +20,56 @@ export default class startScreen extends Phaser.Scene {
 
     this.background = this.add.image(this.xCoord / 2, this.yCoord / 2, 'background').setOrigin(0.5);
     this.add.text(this.xCoord / 2, this.yCoord / 3, "Welcome!", { fontSize: '156px', fontStyle: 'bold',fill: '#fff' }).setOrigin(0.5);
-    const button = this.add.text(this.xCoord / 2, (3 * this.yCoord) / 4, 'Play Game', {
-      fontSize: '64px', 
-      fill: '#000000ff', 
-      backgroundColor: '#aaa2a2ff', 
-      padding: {x: 20, y:10}, 
-      align: 'center'})
-      .setOrigin(0.5)
-      .setInteractive()
+
+      // --- Define the idle animation ---
+    this.anims.create({
+      key: 'frog-idle',
+      frames: this.anims.generateFrameNumbers('frog', { start: 0, end: 3 }),
+      frameRate: 6,
+      repeat: -1
+    });
+
+      // --- Sprite and play the animation ---
+    const frog = this.add.sprite(150, 560, 'frog'); 
+    frog.setScale(4);
+    frog.setDepth(1);
+    frog.play('frog-idle');// start the idle loop
+
+      // --- Play Button ---
+    const playBtn = this.add.image(this.xCoord / 2, (3 * this.yCoord) / 4, 'btnPlay')
+      .setDepth(10)
+      .setScale(0.3)
+      .setScrollFactor(0) 
+      .setInteractive({ useHandCursor: true })
+      .on('pointerover', () => playBtn.setScale(0.33)) // hover = a bit bigger
+      .on('pointerout',  () => playBtn.setScale(0.3))  // leave = back to base
       .on('pointerdown', () => {
         this.scene.start('Game1', {xCoord: this.xCoord, yCoord: this.yCoord});
       });
+      // --- Shop Button ---
+    const trophyBtn = this.add.image(this.xCoord / 3, (3 * this.yCoord) / 4, 'btnTrophy')
+      .setDepth(10)
+      .setScale(0.3)
+      .setScrollFactor(0) 
+      .setInteractive({ useHandCursor: true })
+      .on('pointerover', () => trophyBtn.setScale(0.28)) // hover = a bit bigger
+      .on('pointerout',  () => trophyBtn.setScale(0.25))  // leave = back to base
+    /*  .on('pointerdown', () => {
+        this.scene.start('Game1', {xCoord: this.xCoord, yCoord: this.yCoord});
+      });  
+    */
+
+      // --- Settings Button ---
+    const settingsBtn = this.add.image(this.xCoord / 1.5, (3 * this.yCoord) / 4, 'btnSettings')
+      .setDepth(10)
+      .setScale(0.3)
+      .setScrollFactor(0) 
+      .setInteractive({ useHandCursor: true })
+      .on('pointerover', () => settingsBtn.setScale(0.28)) // hover = a bit bigger
+      .on('pointerout',  () => settingsBtn.setScale(0.25))  // leave = back to base
+    /*  .on('pointerdown', () => {
+        this.scene.start('Game1', {xCoord: this.xCoord, yCoord: this.yCoord});
+      });  
+    */
+    }
   }
-}
