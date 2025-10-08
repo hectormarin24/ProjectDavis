a827f2a09437dc0dea69a8d07e587f8f7e922b18
hectormarin24 <h.marinjr24@gmail.com>
2025-10-08 15:12:41 -0700

    CloseTheLidsGame
    Added 2 trashcan images to assets
    Added 1 background neighborhood
                   image to assets
    Added closeTheLids.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import Game1 from './game1.js';
 import Game2 from './game2.js';
+import closeTheLids from './closeTheLids.js';
 import endScreen from './endScreen.js';
 import startScreen from './startScreen.js';
 
@@ -8,7 +9,7 @@ const config = {
   width: 1000,
   height: 900,
   backgroundColor: '#222222',
-  scene: [startScreen, Game1, Game2, endScreen],
+  scene: [startScreen, Game1, Game2, endScreen, closeTheLids],
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
